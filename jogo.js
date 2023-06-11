6ba62ee8a4d324cf845ade207138570073a859eb
Kaue de Lima Albrecht <134561999+kaue-alb@users.noreply.github.com>
2023-06-11 15:31:09 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,1 +1,63 @@
 
+//variáveis da bolinha
+let xBolinha = 300
+let yBolinha = 200
+let diametro = 20
+let raio = diametro / 2
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5
+let yRaquete = 150
+let larguraRaquete = 10
+let comprimentoRaquete =90
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete();
+  movimentaMinhaRaquete();
+}
+
+function mostraBolinha() {
+circle(xBolinha,yBolinha,diametro);
+}
+
+function movimentaBolinha(){
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda(){
+  if (xBolinha + raio > width || xBolinha - raio < 0){
+    velocidadeXBolinha *= -1;
+    
+  }
+  if (yBolinha + raio > height || yBolinha -raio < 0){
+      velocidadeYBolinha *= -1;
+      }
+}
+
+function mostraRaquete(){
+  
+  rect(xRaquete, yRaquete, larguraRaquete, comprimentoRaquete);
+}
+
+function movimentaMinhaRaquete(){
+  if (keyIsDown(UP_ARROW)){
+    yRaquete -=10;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yRaquete +=10;
+  }
+}
